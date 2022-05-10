82421e66adcff93c5057f950609c73894c6db06d
abubakarsiddique10 <abubakarsiddique7912@gmail.com>
2022-05-10 10:50:17 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+          primary: "blue",
+          "primary-focus": "mediumblue",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
