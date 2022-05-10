b445f611ea16e94516c704910797d75644d54fe9
abubakarsiddique10 <abubakarsiddique7912@gmail.com>
2022-05-10 13:07:29 +0600

    hero section complete

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,9 @@
 import React from "react";
+import Banner from "./Banner";
 const Home = () => {
     return (
         <div>
-            <h1>this is Home page</h1>
+            <Banner></Banner>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,16 @@ module.exports = {
   daisyui: {
     themes: [
       {
-        light: {
-          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
-          primary: "blue",
-          "primary-focus": "mediumblue",
+        mytheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
         },
       },
+      "dark",
+      "cupcake",
     ],
   },
   plugins: [require("daisyui")],
